d9da0c4e0ad0622f3ea59028867f25d18e096372
kotouharuto <haruto.k.0321@gmail.com>
2026-04-09 05:26:40 +0900

    fix: settings.pyの変更

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-+@l9uc!a_2sy%(he@8db7+$kd#=#e$*v2%n1k-2jc8o0=o#w'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
